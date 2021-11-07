6679383e4c8438ad2e5fa99ddd2d82eaac332dec
brxvn <bryangonzalezm@outlook.com>
2021-11-06 20:38:06 -0600

    new screens

---
+++ GymCastillo/View/GridUsuariosUserControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GymCastillo.View {
+    /// <summary>
+    /// Interaction logic for GridUsuariosUserControl.xaml
+    /// </summary>
+    public partial class GridUsuariosUserControl : UserControl {
+        public GridUsuariosUserControl() {
+            InitializeComponent();
+        }
+
+        private void BtnNuevoUsuario_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
