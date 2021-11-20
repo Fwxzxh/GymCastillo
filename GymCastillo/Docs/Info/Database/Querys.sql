0787ecb86b62454904f385af811500b2ae155a7e
Twilight <60190202+juanpabloalegria@users.noreply.github.com>
2021-11-20 12:54:55 -0600

    Update Querys.sql

--- GymCastillo/Docs/Info/Database/Querys.sql
+++ GymCastillo/Docs/Info/Database/Querys.sql
@@ -42,7 +42,8 @@ UPDATE cliente
 SET domicilio=@Domicilio, telefono=@Telefono, condicionespecial=@CondicionEspecial,
 nombrecontacto=@NombreContacto, telefonocontacto=@TelefonoContacto, foto=@Foto,
 activo=@Activo, medioconocio=@MedioConocio, descuento=@Descuento, nino=@Nino,
-idtipocliente=@IdTipoCliente, idpaquete=@IdPaquete;
+idtipocliente=@IdTipoCliente, idpaquete=@IdPaquete
+WHERE idcliente=@IdCliente;
 	-- Editar valores (automatico)
 
 -- Instructor
@@ -61,6 +62,29 @@ INNER JOIN tipoinstructor ti ON i.IdTipoInstructor = ti.IdTipoInstructor
 LEFT JOIN clase c ON c.IdInstructor = i.IdInstructor
 GROUP BY i.IdInstructor;
 	-- Dar de alta
+INSERT INTO instructor
+VALUES (default, @Nombre, @ApellidoPaterno, @ApellidoMaterno,
+	@Domicilio, @FechaNacimiento, @Telefono, @NombreContacto,
+	@TelefonoContacto, @Foto, @FechaUltimoAcceso, @FechaUltimoPago, 
+	@MontoUltimoPago, @HoraEntrada, @HoraSalida, @DiasATrabajar,
+	@DiasTrabajados, @Sueldo, @SueldoADescontar, @IdTipoInstructor);
+	-- Editar valores (usuario)
+UPDATE instructor
+SET domicilio=@Domicilio, telefono=@Telefono, NombreContacto=@NombreContacto,
+telefonocontacto=@TelefonoContacto, foto=@Foto, horaentrada=@HoraEntrada,
+horasalida=@HoraSalida, sueldo=@Sueldo, sueldoadescontar=@SueldoADescontar,
+idtipoinstructor=@IdTipoInstructor
+WHERE idinstructor=@IdInstructor;
+	-- Editar valores (automatico)
 
+-- ClienteRenta
+	-- Consulta de todo lo de ClienteRenta
+	-- Dar de alta
+	-- Editar valores (usuario)
+	-- Editar valores (automatico)
+	
+-- Usuarios
+	-- Consulta de todo lo de Usuarios
+	-- Dar de alta
 	-- Editar valores (usuario)
 	-- Editar valores (automatico)
