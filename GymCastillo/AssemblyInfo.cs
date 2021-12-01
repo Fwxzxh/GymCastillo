fde9d84d7405f473cf23b2443f4437f11b9b8cda
Fxzxzh <jdemeca01@gmail.com>
2021-11-30 19:45:02 -0600

    Another bug bites the dust

--- GymCastillo/AssemblyInfo.cs
+++ GymCastillo/AssemblyInfo.cs
@@ -9,5 +9,4 @@ using System.Windows;
     // app, or any theme specific resource dictionaries)
 )]
 
-//[assembly: log4net.Config.XmlConfigurator(ConfigFile = @"C:\GymCastillo\App.config")]
-[assembly: log4net.Config.XmlConfigurator]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = @"C:\GymCastillo\App.config")]
