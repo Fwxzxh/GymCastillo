a104cc38a1f095feb30e77e91992bf7248905e70
Twilight <60190202+juanpabloalegria@users.noreply.github.com>
2021-12-06 23:49:10 -0600

    Update GenerateTestInfo.sql

--- GymCastillo/Docs/Info/Database/GenerateTestInfo.sql
+++ GymCastillo/Docs/Info/Database/GenerateTestInfo.sql
@@ -73,6 +73,22 @@ VALUES (default, 'Cassandra', 'Carrillo', 'Ledezma', 'Calle Mi Corazon #25 Colon
 , '4420608128', 'Juan Pablo Alegría Escobedo' ,'4426273881', null, CURDATE(), CURDATE(),1500.0, '0700', '1400', 5
 , null, 1500.0, null, 3, 1);
 
+-- Paquetes
+INSERT INTO paquete
+VALUES (default, TRUE, 'Gym', 'Descripción del paquete 1', null, null, 350.0);
+INSERT INTO paquete
+VALUES (default, FALSE, 'Paquete 1 sin gym', 'Descripción del paquete 2',  4, 1, 350);
+INSERT INTO paquete
+VALUES (default, FALSE, 'Paquete 2 sin gym', 'Descripción del paquete 3',  8, 2, 700);
+INSERT INTO paquete
+VALUES (default, FALSE, 'Paquete 3 sin gym', 'Descripción del paquete 4', 12, 3, 1050);
+INSERT INTO paquete
+VALUES (default, TRUE, 'Paquete 4 con gym', 'Descripción del paquete 5', 4, 1, 700);
+INSERT INTO paquete
+VALUES (default, TRUE, 'Paquete 5 con gym', 'Descripción del paquete 6', 8, 2, 1050);
+INSERT INTO paquete
+VALUES (default, TRUE, 'Paquete 6 con gym', 'Descripción del paquete 7', 12, 3, 1400);
+
 -- Clases
 INSERT INTO clase
 VALUES (default, 'Natación', 'Se enseña a nadar', 24, TRUE, 1, 1, 1);
@@ -90,22 +106,6 @@ INSERT INTO horario
 VALUES (default, 4, '0700', '0800', null, 1);
 INSERT INTO horario
 VALUES (default, 5, '0700', '0800', null, 1);
-
--- Paquetes
-INSERT INTO paquete
-VALUES (default, TRUE, 'Gym', 'Descripción del paquete 1', null, null, 350.0);
-INSERT INTO paquete
-VALUES (default, FALSE, 'Paquete 1 sin gym', 'Descripción del paquete 2',  4, 1, 350);
-INSERT INTO paquete
-VALUES (default, FALSE, 'Paquete 2 sin gym', 'Descripción del paquete 3',  8, 2, 700);
-INSERT INTO paquete
-VALUES (default, FALSE, 'Paquete 3 sin gym', 'Descripción del paquete 4', 12, 3, 1050);
-INSERT INTO paquete
-VALUES (default, TRUE, 'Paquete 4 con gym', 'Descripción del paquete 5', 4, 1, 700);
-INSERT INTO paquete
-VALUES (default, TRUE, 'Paquete 5 con gym', 'Descripción del paquete 6', 8, 2, 1050);
-INSERT INTO paquete
-VALUES (default, TRUE, 'Paquete 6 con gym', 'Descripción del paquete 7', 12, 3, 1400);
 
 -- Lockers
 INSERT INTO locker
