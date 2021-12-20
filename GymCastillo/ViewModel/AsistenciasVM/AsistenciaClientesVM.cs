e10681feb3c10ec2a61cf05de512dd61ba5c76b6
brxvn <bryangonzalezm@outlook.com>
2021-12-20 00:14:04 -0600

    asistencia clientes half done

---
+++ GymCastillo/ViewModel/AsistenciasVM/AsistenciaClientesVM.cs
@@ -0,0 +1,34 @@
+﻿using GymCastillo.Model.DataTypes.Otros;
+using GymCastillo.Model.DataTypes.Personal;
+using GymCastillo.Model.Init;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymCastillo.ViewModel.AsistenciasVM {
+    public class AsistenciaClientesVM : INotifyPropertyChanged {
+
+        private Cliente cliente = new();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Cliente Cliente {
+            get { return cliente; }
+            set { cliente = value;
+                OnPropertyChanged(nameof(Cliente));
+            }
+        }
+
+        public AsistenciaClientesVM(Asistencia asistencia) {
+            Cliente = asistencia.DatosCliente;            
+        }
+
+        private void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ GymCastillo/ViewModel/AsistenciasVM/HomeAsistenciasVM.cs
@@ -0,0 +1,59 @@
+﻿using GalaSoft.MvvmLight.Command;
+using GymCastillo.Model.DataTypes.Otros;
+using GymCastillo.Model.Helpers;
+using GymCastillo.View.AsistenciasView;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymCastillo.ViewModel.AsistenciasVM {
+    public class HomeAsistenciasVM : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RelayCommand asistenciaCliente { get; set; }
+        public RelayCommand asistenciaInstructor { get; set; }
+
+        private Asistencia asistencia = new();
+
+        public Asistencia Asistencia {
+            get { return asistencia; }
+            set { asistencia = value;
+                OnPropertyChanged(nameof(Asistencia));
+            }
+        }
+
+        private int id;
+
+        public int Id {
+            get { return id; }
+            set { id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+
+        public HomeAsistenciasVM() {
+
+            asistenciaCliente = new RelayCommand(AsistenciaClienteView);
+
+        }
+
+        private void AsistenciaClienteView() {
+            AsistenciasHelper helper = new();
+            Asistencia.Tipo = 1;
+            if (helper.CheckId(Asistencia)) {
+                if (helper.CheckEntrada(Asistencia).Item1) {
+                    Asistencia = helper.CheckEntrada(Asistencia).Item2;
+                    AsistenciaClienteWindow window = new(Asistencia);
+                    window.ShowDialog();
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
