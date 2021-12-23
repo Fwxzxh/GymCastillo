3bbe1b0a8cfd6d3d524f6d20fb537ad7d43c622b
brxvn <bryangonzalezm@outlook.com>
2021-12-23 13:57:54 -0600

    only numbers fields inventario

--- GymCastillo/View/VentasScreensView/InventarioView/InventarioUC.xaml.cs
+++ GymCastillo/View/VentasScreensView/InventarioView/InventarioUC.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,11 @@ namespace GymCastillo.View.VentasScreensView.InventarioView {
     public partial class InventarioUC : UserControl {
         public InventarioUC() {
             InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
