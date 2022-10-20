98e0e9bdc1dd10ef73b0b3e2f886c92d452abdf1
brxvn <bryangonzalezm@outlook.com>
2022-10-20 09:31:58 -0500

    impresión de tickets resuelto

--- GymCastillo/ViewModel/PagosScreensVM/IngresosVM.cs
+++ GymCastillo/ViewModel/PagosScreensVM/IngresosVM.cs
@@ -319,8 +319,8 @@ namespace GymCastillo.ViewModel.PagosScreensVM {
             }
             
             await PagosHelper.NewIngreso(ingresos, meses: NoMeses + 1);
-            //tickets = new PrintTickets($"Pago {paquete.NombrePaquete}", ingresos.Monto, GetInitData.GetMonthMovNumerator(), ingresos.MontoRecibido, idCliente: cliente.Id);
-            //tickets = new PrintTickets($"Pago {paquete.NombrePaquete}", ingresos.Monto, GetInitData.GetMonthMovNumerator(), ingresos.MontoRecibido, idCliente: cliente.Id);
+            tickets = new PrintTickets($"Pago {paquete.NombrePaquete}", ingresos.Monto, GetInitData.GetMonthMovNumerator(), ingresos.MontoRecibido, idCliente: cliente.Id);
+            tickets = new PrintTickets($"Pago {paquete.NombrePaquete}", ingresos.Monto, GetInitData.GetMonthMovNumerator(), ingresos.MontoRecibido, idCliente: cliente.Id);
             DoublePagoProtection.Item1 = horaNuevaRegistro;
             DoublePagoProtection.Item2 = Cliente.Id;
             
